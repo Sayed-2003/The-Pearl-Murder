b8d3979f7c26802416d178c1ec6a621c4b7ab6c6
Sayed-2003 <baqerglopgamer@gmail.com>
2026-08-08 07:35:07 +0300

    added navigation in the game to go back or start the game

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("The Pearl Murder is ready!");
+console.log("The Pearl Murder");
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const landingPageEl = document.querySelector('#landing-page')
+const briefingPageEl = document.querySelector('#briefing-page')
+const startBtnEl = document.querySelector('#start-btn')
+const backBtnEl = document.querySelector('#back-btn')
+
+
+console.log(landingPageEl)
+console.log(briefingPageEl)
+console.log(backBtnEl)
+console.log(startBtnEl)
+/*-------------------------------- Functions --------------------------------*/
+function showBriefingPage(){
+    landingPageEl.style.display = 'none'
+    briefingPageEl.style.display = 'flex'
+}
+
+function showLandingPage(){
+    briefingPageEl.style.display = 'none'
+    landingPageEl.style.display = 'flex'
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+startBtnEl.addEventListener('click',showBriefingPage)
+
+backBtnEl.addEventListener('click',showLandingPage)
+
+
