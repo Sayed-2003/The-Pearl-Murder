8ac861152fad34fe371430786baed3b3ac17704d
Sayed-2003 <baqerglopgamer@gmail.com>
2026-08-10 04:14:13 +0300

    Connect case briefing to Hassan office scene

--- js/app.js
+++ js/app.js
@@ -13,12 +13,16 @@ const landingPageEl = document.querySelector('#landing-page')
 const briefingPageEl = document.querySelector('#briefing-page')
 const startBtnEl = document.querySelector('#start-btn')
 const backBtnEl = document.querySelector('#back-btn')
+const beginInvestigationBtn = document.querySelector('#begin-investigation-btn')
+const firstScene = document.querySelector('#scene1-page')
 
 
 console.log(landingPageEl)
 console.log(briefingPageEl)
 console.log(backBtnEl)
 console.log(startBtnEl)
+console.log(beginInvestigationBtn)
+console.log(firstScene)
 /*-------------------------------- Functions --------------------------------*/
 function showBriefingPage(){
     landingPageEl.style.display = 'none'
@@ -30,9 +34,15 @@ function showLandingPage(){
     landingPageEl.style.display = ''
 }
 
+function showScene1(){
+    briefingPageEl.style.display = 'none'
+    firstScene.style.display = 'flex'
+}
+
 /*----------------------------- Event Listeners -----------------------------*/
 startBtnEl.addEventListener('click',showBriefingPage)
 
 backBtnEl.addEventListener('click',showLandingPage)
 
+beginInvestigationBtn.addEventListener('click',showScene1)
 
