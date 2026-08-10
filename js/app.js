71d576df7d525763abb95ab51addb8b238299dda
Sayed-2003 <baqerglopgamer@gmail.com>
2026-08-10 04:41:20 +0300

    added scene 1 iteraction with investigation

--- js/app.js
+++ js/app.js
@@ -13,14 +13,21 @@ const landingPageEl = document.querySelector('#landing-page')
 const briefingPageEl = document.querySelector('#briefing-page')
 const startBtnEl = document.querySelector('#start-btn')
 const backBtnEl = document.querySelector('#back-btn')
+
 const beginInvestigationBtn = document.querySelector('#begin-investigation-btn')
 const firstScene = document.querySelector('#scene1-page')
 
 
+const officeBtn = document.querySelector('#office-btn')
+const deskBtn = document.querySelector('#desk-btn')
+const ObserveText = document.querySelector('#scene1-observation')
+
+// testing the cache 
 console.log(landingPageEl)
 console.log(briefingPageEl)
 console.log(backBtnEl)
 console.log(startBtnEl)
+
 console.log(beginInvestigationBtn)
 console.log(firstScene)
 /*-------------------------------- Functions --------------------------------*/
@@ -39,10 +46,21 @@ function showScene1(){
     firstScene.style.display = 'flex'
 }
 
+function examineOffice(){
+    ObserveText.textContent = "The office shows signs of a struggle. Papers are scattered across the floor."
+}
+
+function examineDesk(){
+    ObserveText.textContent =  "You discover an unfinished letter mentioning a serious family dispute."
+}
 /*----------------------------- Event Listeners -----------------------------*/
 startBtnEl.addEventListener('click',showBriefingPage)
 
 backBtnEl.addEventListener('click',showLandingPage)
 
 beginInvestigationBtn.addEventListener('click',showScene1)
+
+officeBtn.addEventListener('click',examineOffice)
+
+deskBtn.addEventListener('click', examineDesk)
 
