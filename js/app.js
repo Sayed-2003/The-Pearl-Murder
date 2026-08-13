84a8db66ec0ddb97213acd113adcbe72dd66c274
Sayed-2003 <baqerglopgamer@gmail.com>
2026-08-13 07:30:40 +0300

    fixed some styling issues

--- js/app.js
+++ js/app.js
@@ -23,9 +23,9 @@ const deskBtn = document.querySelector('#desk-btn')
 const ObserveText = document.querySelector('#scene1-observation')
 
 // scene 2
-const continueToScene2Btn =document.querySelector('#continue-to-hall')
+const continueToScene2Btn = document.querySelector('#continue-to-hall')
 const seconedScene = document.querySelector('#scene2-page')
-const ObserveText2= document.querySelector('#scene2-observation')
+const ObserveText2 = document.querySelector('#scene2-observation')
 const askHasanEl = document.querySelector('#askHasan')
 const askMohamedEl = document.querySelector('#askMohammed')
 const askYosufEl = document.querySelector('#askYosuf')
@@ -68,152 +68,152 @@ console.log(firstScene)
 console.log(continueToScene3Btn)
 console.log(thirdscene)
 /*-------------------------------- Functions --------------------------------*/
-function showBriefingPage(){
+function showBriefingPage() {
     landingPageEl.style.display = 'none'
     briefingPageEl.style.display = 'flex'
 }
 
-function showLandingPage(){
+function showLandingPage() {
     briefingPageEl.style.display = 'none'
     landingPageEl.style.display = ''
 }
 
 // scene 1
-function showScene1(){
+function showScene1() {
     console.log('in scene 1')
     briefingPageEl.style.display = 'none'
     firstScene.style.display = 'flex'
 }
 
-function examineOffice(){
+function examineOffice() {
     ObserveText.textContent = " The office shows signs of a struggle. Several papers have been knocked onto the floor, and a decorative letter opener lies nearby"
 
 }
 
-function examineDesk(){
-    ObserveText.textContent =" You discover an unfinished letter. Hassan wrote about serious disagreements involving his family and business"
+function examineDesk() {
+    ObserveText.textContent = " You discover an unfinished letter. Hassan wrote about serious disagreements involving his family and business"
 }
 
 // scene 2
-function showScene2(){
+function showScene2() {
     console.log('in scene 2')
     firstScene.style.display = 'none'
     seconedScene.style.display = 'flex'
 }
 
-function askHasan(){
+function askHasan() {
     ObserveText2.textContent = "Hassan had been worried lately. He was having problems with both his family and his business."
 }
 
-function askMohammed(){
+function askMohammed() {
     ObserveText2.textContent = "I heard Hassan arguing with Mohammed the night before he died. It sounded serious."
 }
 
-function askYosuf(){
-    ObserveText2.textContent  = "Hassan and Yousef had been arguing about money for weeks. Hassan didn't trust him like he used to."
+function askYosuf() {
+    ObserveText2.textContent = "Hassan and Yousef had been arguing about money for weeks. Hassan didn't trust him like he used to."
 }
 
 // scene 3
-function showScene3(){
+function showScene3() {
     console.log('in scene 3')
 
     seconedScene.style.display = 'none'
     thirdscene.style.display = 'flex'
 }
 
-function financialRecords(){
+function financialRecords() {
     ObserveText3.textContent = "You discover that Hassan and Yousef recently lost a large amount of money in a failed business deal."
 }
 
-function hassanNotes(){
+function hassanNotes() {
     ObserveText3.textContent = "Hassan wrote that he planned to end his partnership with Yousef. The note makes Yousef appear to have a strong motive."
 }
 
 // scene 4
 
-function showScene4(){
+function showScene4() {
     thirdscene.style.display = 'none'
     fourthscene.style.display = 'flex'
 }
 
-function searchRoom(){
+function searchRoom() {
     ObserveText4.textContent = "You discover a letter showing that Hassan planned to greatly reduce Mohammed's inheritance."
 }
 
-function inspectDesk(){
+function inspectDesk() {
     ObserveText4.textContent = "You find a note written by Mohammed after an argument with his father. His anger toward Hassan is clear."
 }
 
 // scene 5
-function showLastScene(){
+function showLastScene() {
     fourthscene.style.display = 'none'
     lastScene.style.display = 'flex'
 }
 
-function finalAccusation(event){
-    if(event.target === accuseMohammedEl){
+function finalAccusation(event) {
+    if (event.target === accuseMohammedEl) {
         message = "Detective Abdulla confronts Mohammed with what he discovered. Mohammed's story begins to fall apart, and he eventually admits his responsibility for Hassan's death."
-        accusationImageEl.setAttribute('src','./Assets/images/absolute-cinema.png')
+        accusationImageEl.setAttribute('src', './Assets/images/absolute-cinema.png')
         accusationEl.textContent = message
         lastScene.style.display = 'none'
         accusationScene.style.display = 'flex'
-    }else if(event.target === accuseYousefEl){
+    } else if (event.target === accuseYousefEl) {
         message = "Detective Abdulla accuses Yousef. Although Yousef had a motive, the conclusion was wrong. The investigation collapses while the real killer remains free."
-         falseAccusationEl.setAttribute('src','./Assets/images/false-accusation.png')
+        falseAccusationEl.setAttribute('src', './Assets/images/false-accusation.png')
         accusationEl.textContent = message
         lastScene.style.display = 'none'
         accusationScene.style.display = 'flex'
-    }else{
-        message= "Detective Abdulla delays making an accusation. With the investigation stalled, Mohammed takes the opportunity to disappear before the truth can be proven."
-         falseAccusationEl.setAttribute('src','./Assets/images/false-accusation.png')
+    } else {
+        message = "Detective Abdulla delays making an accusation. With the investigation stalled, Mohammed takes the opportunity to disappear before the truth can be proven."
+        falseAccusationEl.setAttribute('src', './Assets/images/false-accusation.png')
         accusationEl.textContent = message
         lastScene.style.display = 'none'
         accusationScene.style.display = 'flex'
     }
 }
 
-function backToHomeBtn(){
+function backToHomeBtn() {
     message = null
-    accusationImageEl.setAttribute('src','')
-    falseAccusationEl.setAttribute('src','')
-     landingPageEl.style.display = ''
-     accusationScene.style.display = 'none'
+    accusationImageEl.setAttribute('src', '')
+    falseAccusationEl.setAttribute('src', '')
+    landingPageEl.style.display = ''
+    accusationScene.style.display = 'none'
 
 }
 /*----------------------------- Event Listeners -----------------------------*/
-startBtnEl.addEventListener('click',showBriefingPage)
+startBtnEl.addEventListener('click', showBriefingPage)
 
-backBtnEl.addEventListener('click',showLandingPage)
+backBtnEl.addEventListener('click', showLandingPage)
 
 // scene1
-beginInvestigationBtn.addEventListener('click',showScene1)
+beginInvestigationBtn.addEventListener('click', showScene1)
 
-officeBtn.addEventListener('click',examineOffice)
+officeBtn.addEventListener('click', examineOffice)
 
 deskBtn.addEventListener('click', examineDesk)
 
 // scene 2
 continueToScene2Btn.addEventListener('click', showScene2)
 
-askHasanEl.addEventListener('click',askHasan)
+askHasanEl.addEventListener('click', askHasan)
 
-askMohamedEl.addEventListener('click',askMohammed)
+askMohamedEl.addEventListener('click', askMohammed)
 
-askYosufEl.addEventListener('click' , askYosuf)
+askYosufEl.addEventListener('click', askYosuf)
 
 // scene 3
 continueToScene3Btn.addEventListener('click', showScene3)
-financialRecordsEl.addEventListener('click',financialRecords)
-hassanNotesEl.addEventListener('click',hassanNotes)
+financialRecordsEl.addEventListener('click', financialRecords)
+hassanNotesEl.addEventListener('click', hassanNotes)
 
 // scene 4
-continueToScene4Btn.addEventListener('click',showScene4)
-searchRoomEl.addEventListener('click',searchRoom)
-inspectDeskEl.addEventListener('click',inspectDesk)
+continueToScene4Btn.addEventListener('click', showScene4)
+searchRoomEl.addEventListener('click', searchRoom)
+inspectDeskEl.addEventListener('click', inspectDesk)
 
 // scene5 
-continueToScene5Btn.addEventListener('click',showLastScene)
-accuseMohammedEl.addEventListener('click',finalAccusation)
-accuseYousefEl.addEventListener('click',finalAccusation)
+continueToScene5Btn.addEventListener('click', showLastScene)
+accuseMohammedEl.addEventListener('click', finalAccusation)
+accuseYousefEl.addEventListener('click', finalAccusation)
 moreEvidenceEl.addEventListener('click', finalAccusation)
-backToHomeEl.addEventListener('click',backToHomeBtn)
+backToHomeEl.addEventListener('click', backToHomeBtn)
